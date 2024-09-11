81ad651f5a8fe97d6304892a25286dba97a0f3f2
ruiolive <ruipedro.pires999@gmail.com>
2024-09-11 11:07:14 +0100

    Refactor file paths in docker-compose.yaml, Makefile, and .env

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -3,10 +3,8 @@
 cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-sleep 5
 ./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=ruiolive --dbpass=12345678 --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
-./wp-cli.phar user create gues guest@guest.com --role=subscriber --user_pass=guest123 --allow-rootphp
-echo "define('FS_METHOD', 'direct');" >> /var/www/html/wp-config.php
+./wp-cli.phar config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost=mariadb --allow-root
+./wp-cli.phar core install --url=localhost --title=inception --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --allow-root
+./wp-cli.phar user create --user=${WP_USER} --user_pass=${WP_PASSWORD} --email=${WP_EMAIL} --role=subscriber --allow-root
 php-fpm7.4 -F
