849d4f14010e210488a14f5ba9357e2d813aaf81
ruiolive <ruipedro.pires999@gmail.com>
2024-09-18 11:20:20 -0400

    Refactor entryscript.sh to create a guest user with specified credentials

--- srcs/requirements/wordpress/conf/entryscript.sh
+++ srcs/requirements/wordpress/conf/entryscript.sh
@@ -10,7 +10,7 @@ chmod +x wp-cli.phar
 ./wp-cli.phar config set WP_REDIS_HOST redis --type=constant --allow-root
 ./wp-cli.phar config set WP_REDIS_PORT 6379 --type=constant --raw --allow-root
 ./wp-cli.phar core install --url=localhost --title=inception --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --allow-root
-./wp-cli.phar user create --user=${WP_USER} --user_pass=${WP_PASSWORD} --email=${WP_EMAIL} --role=subscriber --allow-root
+./wp-cli.phar user create guest ${WP_USER} ${WP_PASSWORD} ${WP_EMAIL} --role=subscriber --allow-root
 ./wp-cli.phar plugin install redis-cache --activate --allow-root
 ./wp-cli.phar redis enable --allow-root
 php-fpm7.4 -F
